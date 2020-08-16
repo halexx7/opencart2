342f1d219bdfea9b4f958d38eee869e255baa848
Aleksey Khlybov <halexx7@gmail.com>
2020-08-16 20:33:16 +0300

    Install OpenCART2

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.loc/admin/');
+define('HTTP_CATALOG', 'http://opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/docs/opencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
